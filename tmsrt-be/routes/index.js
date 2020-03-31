789af5f77c84920b340806ecdf611eab8e915243
Juha-Matti Tirila <jtirila@gmail.com>
2020-03-31 13:36:32 +0300

    Remove dummy route.

--- tmsrt-be/routes/index.js
+++ tmsrt-be/routes/index.js
@@ -6,8 +6,4 @@ router.get('/', function(req, res, next) {
     res.send("Hello tmsrt-be user! No content for now.");
 });
 
-router.get('/test', function(req, res, next) {
-    res.send("How about this?");
-});
-
 module.exports = router;
