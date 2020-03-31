4905b1cf4e7184ec892f0ac993dff429da10dc8a
Juha-Matti Tirila <jtirila@gmail.com>
2020-03-31 13:36:00 +0300

    Remove index.html, add dummy test route.

--- tmsrt-be/routes/index.js
+++ tmsrt-be/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET root */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.send("Hello tmsrt-be user! No content for now.");
+});
+
+router.get('/test', function(req, res, next) {
+    res.send("How about this?");
 });
 
 module.exports = router;
