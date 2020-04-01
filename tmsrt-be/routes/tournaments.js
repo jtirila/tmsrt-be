9a213ee7145d65b6a162f9c8dd6f10bf0aa3ecf8
Juha-Matti Tirila <jtirila@gmail.com>
2020-04-01 09:07:47 +0300

    A first attempt at having a minimal API

--- tmsrt-be/routes/tournaments.js
+++ tmsrt-be/routes/tournaments.js
@@ -1,11 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+// This is probably not the place for connecting to the database or defining the
+// models. Just trying things out for now.
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const tournamentSchema = new mongoose.Schema({name: String, location: String});
+const Tournament = mongoose.model('Tournament', tournamentSchema);
+
+
 /* GET tournaments listing. */
 router.get('/', function(req, res, next) {
-  // FIXME  
-    res.json([{name: "My Tournament", location: "Ice Rink"},
-	      {name: "Jack's Tournament", location: "Pittbull Arena"}]);
+    // FIXME
+    Tournament.find((err, docs) => {
+	if(err) return console.error(err);
+        res.json(docs);
+    })
+
 });
+
+
+router.post('/', function(req, res, next) {
+    const newTournament = req.body;
+    // Beware, no error handling
+    const tournament = new Tournament(newTournament);
+    tournament.save().then(() => console.log('Tournament saved'));
+});
+
 
 module.exports = router;
