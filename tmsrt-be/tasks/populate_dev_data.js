db96da55e048e237e471dcc65323b36bb6a9faee
Juha-Matti Tirila <jtirila@gmail.com>
2020-04-16 08:19:29 +0300

    Add note about database reset; modify populater

--- tmsrt-be/tasks/populate_dev_data.js
+++ tmsrt-be/tasks/populate_dev_data.js
@@ -1,13 +1,22 @@
 const Tournament = require('../models/tournament.js');
 const Team = require('../models/team.js');
 
+
+
 handle_error = err => console.log(`Error: ${err}`);
 
-module.exports = function(mongoose) {
+let populate = function(mongoose) {
+    // This is probably not the place for connecting to the database or defining the
+    // models. Just trying things out for now.
+    const mongoose = require('mongoose');
+    mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+    
     Tournament.create({
 	name: "Pond Hockey 2021",
 	location: "Raksila", handleError
     });
 
-    Team.create({name: "OHK"}, handleError)
+    Team.create({name: "Burritos"}, handleError)
 }
+
+populate();
